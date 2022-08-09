c44bf69addb2b077924bdc0b88ab0676d8dc55e6
Maxim Sozinov <max.sozinov@cupcakedev.com>
2022-08-09 21:27:28 +0700

    add server

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { createServer } from 'http';
+
+import { port } from './config';
+
+const routing = {
+  '/api': async () => JSON.stringify({ api: true }),
+  // '/test': async () => {
+  //   let resData = { from: 'api' };
+  //   const response = await fetch(`${authApiUrl}/test`);
+  //   const parsedData = await response.json();
+  //   resData = { ...parsedData, ...resData };
+  //   return JSON.stringify(resData);
+  // },
+};
+
+console.log(process.env.PORT);
+
+
+const server = createServer(async (req, res) => {
+  const handler = routing[req.url];
+  if (!handler) {
+    return res.end('Not found');
+  }
+  const result = await handler();
+  res.end(result);
+});
+server.listen(port, () => {
+  console.log(`Started server at port ${port}`);
+});
