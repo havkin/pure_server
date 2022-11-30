9bb81fa9246bdbaf7f1e57e999ac25f0142d53e5
Max Sozinov <max.sozinov@cupcakedev.com>
2022-11-30 15:18:01 +0700

    set fastify

---
+++ index.ts
@@ -0,0 +1,18 @@
+import Fastify from 'fastify';
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (error) {
+    fastify.log.error(error);
+    process.exit(1);
+  }
+};
+void start();
