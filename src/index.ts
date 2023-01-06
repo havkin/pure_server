bb54d704e7a35546474e8dcd0dae8c2bb324b9f9
Maxim Sozinov <max.sozinov@cupcakedev.com>
2023-01-06 18:16:31 +0700

    add static

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { createServer } from 'node:http';
 
 import { port } from './config';
+import { MIME_TYPES, prepareFile } from './config/lib';
 import { router } from './router';
 import type { RouteHandler } from './types';
 
@@ -15,6 +16,16 @@ export const server = createServer(async (req, res) => {
   const [controller, ...paths] = url.pathname
     .split('/')
     .filter((path) => path !== '');
+
+  if (!controller || controller.includes('.')) {
+    const file = await prepareFile(req.url);
+    const statusCode = file.found ? 200 : 404;
+    const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.default;
+    res.writeHead(statusCode, { 'Content-Type': mimeType });
+    file.stream.pipe(res);
+    console.log(`${req.method} ${req.url} ${statusCode}`);
+    return;
+  }
 
   console.log(req.method, url.pathname);
 
